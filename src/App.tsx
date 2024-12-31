d2f09f289b3a4d014e6a14f0949a6bb4e86c90a2
shayse <shay@resight.io>
2024-12-31 10:14:31 +0200

    add vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import GitHubGrid from './components/GitHubGrid';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  return <GitHubGrid />;
+  return (
+    <>
+      <GitHubGrid />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
